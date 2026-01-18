e7323fbd1e3b3282baf7ffc03c0ed8e16a8410f2
Dahyun <jduh1020@naver.com>
2026-01-18 23:40:40 +0900

    draw graph

---
+++ steps/step23.py
@@ -0,0 +1,14 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+
+z = x+y
+z.backward()
+
+print(x.grad, y.grad)
